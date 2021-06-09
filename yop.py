313ac7a71ad7c2fdebccda849d647101d88a11fb
DillonGrabskiInquiries <sahasrahlainquiries@gmail.com>
2021-06-09 01:21:18 -0500

    ya

---
+++ yop.py
@@ -0,0 +1,70 @@
+x = [ [5,2,3], [10,8,9] ] 
+students = [
+    {'first_name':  'Michael', 'last_name' : 'Jordan'},
+    {'first_name' : 'John', 'last_name' : 'Rosales'}
+]
+sports_directory = {
+    'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
+    'soccer' : ['Messi', 'Ronaldo', 'Rooney']
+}
+z = [ {'x': 10, 'y': 20} ]
+
+def sorter1(input):
+    x [1][0] = 15
+    print(x)
+
+sorter1(x)
+
+students = [
+        {'first_name':  'Michael', 'last_name' : 'Jordan'},
+        {'first_name' : 'John', 'last_name' : 'Rosales'},
+        {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+        {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
+# should output: (it's okay if each key-value pair ends up on 2 separate lines;
+# bonus to get them to appear exactly as below!)
+# first_name - Michael, last_name - Jordan
+# first_name - John, last_name - Rosales
+# first_name - Mark, last_name - Guillen
+# first_name - KB, last_name - Tonel
+
+def iterateDictionary(list):
+    print(f"{list[0]}, {list[1]}, {list[2]}, {list[3]}")
+
+
+iterateDictionary(students) 
+
+def iterateDictionary2(name, list):
+    print(f"{list[0][name]}, {list[1][name]}, {list[2][name]}, {list[3][name]}")
+
+iterateDictionary2('first_name', students)
+
+dojo = {
+    'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+    'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+}
+
+# # output:
+# 7 LOCATIONS
+# San Jose
+# Seattle
+# Dallas
+# Chicago
+# Tulsa
+# DC
+# Burbank
+    
+# 8 INSTRUCTORS
+# Michael
+# Amy
+# Eduardo
+# Josh
+# Graham
+# Patrick
+# Minh
+# Devon
+
+def printInfo(dict):
+    print(f"{len(dict[0])}, {dict[0]}, {len(dict[1])}, {dict[1]}")
+
+printInfo(dojo)
